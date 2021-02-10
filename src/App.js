4ac5bac47de5aa639c7ff4db5c568a56d61747af
unknown <hassandahab11@gmail.com>
2021-02-10 16:53:28 +0100

    movie compnenets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrouserRoute, Link, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Favorite from "./components/Favorite";
 import PopularBattle from "./components/PopularBattele";
 import Popular from "./components/Popular";
@@ -10,9 +10,28 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-     <BrouserRoute>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/weekly">weekly</Link></li>
+            <li><Link to="/weekly-battle">weekly-battle</Link></li>
+            <li><Link to="/popular">popular</Link></li>
+            <li><Link to="/popular-battle">popular-battle</Link></li>
+            <li><Link to="/favorite">favorite</Link></li>
+          </ul>
+        </nav>
 
-     </BrouserRoute>
+        <Switch>
+          <Route exact path="/" component={App} ></Route>
+          <Route path="/weekly" component={Weekly}></Route>
+          <Route path="/weekly-battle" component={WeeklyBattle}></Route>
+          <Route path="/popular" component={Popular}></Route>
+          <Route path="/popular-battle" component={PopularBattle}></Route>
+          <Route path="/favorite" component={Favorite}></Route>
+        </Switch>
+
+      </BrowserRouter>
     );
   }
 }
