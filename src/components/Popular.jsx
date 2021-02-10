09cd47f59b6674045ed6ac36f4658cf4a7b3ca65
unknown <hassandahab11@gmail.com>
2021-02-10 17:30:49 +0100

    movie new vrso

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class Popular extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+
+            movies: [],
+
+        }
+    }
+
+
+    componentDidMount={
+        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={46a12a0dcce2af5d37ce67d499098b1f}")
+    }
     render() {
         return(
             <h1>
